a89dc969093d2b79d5a208b902b85c849336922a
wi16b046 <wi16b045@technikum-wien.at>
2017-10-05 20:26:19 +0200

    Commit Message

---
+++ CodingDojo1/CustomStack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo1
+{
+    class CustomStack<T>
+    {
+        private T DataObject { get; set; }
+        private CustomStack<T> NextElement { get; set; }
+
+        public CustomStack() : this(default(T))
+        {
+        }
+
+        public CustomStack(T dataObject)
+        {
+            DataObject = dataObject;
+            NextElement = null;
+        }
+
+        public void Push(T dataObject)
+        {
+            if (EqualityComparer<T>.Default.Equals(DataObject, default(T)))
+            {
+                DataObject = dataObject;
+            }
+            else
+            {
+                CustomStack<T> currentElement = new CustomStack<T>(DataObject);
+                currentElement.NextElement = NextElement;
+                NextElement = currentElement;
+                DataObject = dataObject;
+            }   
+        }
+
+        public T Pop()
+        {
+            T tempObject = DataObject;
+
+            if(NextElement == null)
+            {
+                DataObject = default(T);
+            }
+            else
+            {
+                DataObject = NextElement.DataObject;
+                NextElement = NextElement.NextElement;
+            }
+
+            return tempObject;
+        }
+
+        public T Peak()
+        {
+            return DataObject;
+        }
+
+    }
+}
---
+++ CodingDojo1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stringStack = new CustomStack<string>();
+            string test = stringStack.Pop();
+            if(test == null)
+            {
+                Console.WriteLine("Case 1: Erfolgreich, retourniert null");
+            }
+            else
+            {
+                Console.WriteLine("Case 1: Fehler!");
+            }
+            test = stringStack.Peak();
+            if (test == null)
+            {
+                Console.WriteLine("Case 2: Erfolgreich, retourniert null");
+            }
+            else
+            {
+                Console.WriteLine("Case 2: Fehler!");
+            }
+            stringStack.Push("Test");
+            test = stringStack.Peak();
+            String test1 = stringStack.Peak();
+            if(test != null && test1 != null && test.Equals(test1))
+            {
+                Console.WriteLine("Case 3: Erfolgreich -> " + test);
+            }
+            else
+            {
+                Console.WriteLine("Case 3: Fehler");
+            }
+            test = stringStack.Pop();
+            test1 = stringStack.Peak();
+            string test2 = stringStack.Pop();
+            if(test != null && string.IsNullOrEmpty(test1) && string.IsNullOrEmpty(test2))
+            {
+                Console.WriteLine("Case 4: Erfolgreich");
+            }
+            else
+            {
+                Console.WriteLine("Case 4: Fehler -> " + test + " -> " + test1 + " -> " + test2);
+            }
+            stringStack.Push("A");
+            stringStack.Push("B");
+            stringStack.Push("C");
+            Console.WriteLine("Case 5:");
+            do
+            {
+                if(stringStack.Peak() == null)
+                {
+                    Console.WriteLine("Output: null");
+                }
+                Console.WriteLine("Output: " + stringStack.Pop());
+            }
+            while (stringStack.Peak() != null);
+
+            stringStack.Push("D");
+            stringStack.Push("E");
+            stringStack.Push("F");
+            Console.WriteLine("Case 6:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Output: " + stringStack.Peak());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
